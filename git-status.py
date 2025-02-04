10a210b43754f696b3fb1665e1ee9013447841ee
Shixian Li <lsx7@sina.com>
2025-02-04 08:35:36 +0800

    Perf: No more using "os.chdir"

--- git-status.py
+++ git-status.py
@@ -2,17 +2,13 @@ import os
 import textwrap
 import subprocess
 
-popen = lambda cmd: subprocess.Popen(cmd, -1, None, -1, -1, -1, shell=True, encoding='u8').stdout.read()
+popen = lambda cmd, cwd=None: subprocess.Popen(cmd, -1, None, -1, -1, -2, shell=True, cwd=cwd).stdout.read().decode()
 
-os.chdir('..')
-
-for name in os.listdir():
-    if os.path.isdir(name):
-        os.chdir(name)
-        log = popen('git.exe log --oneline origin...head')
+for name in os.listdir('..'):
+    if os.path.isdir(f'../{name}') and os.path.isdir(f'../{name}/.git'):
+        log = popen('git.exe log --oneline origin...head', f'../{name}').strip()
         if log:
             print(f"Repo '{name}':")
-            print(textwrap.indent(log, '  '))
-        os.chdir('..')
+            print(textwrap.indent(log, '  ') + '\n')
 
 os.system('pause')
--- github-pull-all.py
+++ github-pull-all.py
@@ -6,19 +6,19 @@ import subprocess
 import urllib.request
 
 
-popen = lambda cmd: subprocess.Popen(cmd, -1, None, -1, -1, -2, shell=True).stdout.read().decode()
+popen = lambda cmd, cwd=None: subprocess.Popen(cmd, -1, None, -1, -1, -2, shell=True, cwd=cwd).stdout.read().decode()
 
 
-def RetryPopen(cmd):
+def RetryPopen(cmd, cwd=None):
     while True:
-        ret = popen(cmd)
+        ret = popen(cmd, cwd)
         print(textwrap.indent(ret.rstrip(), '  '))
         if not re.search(r'^fatal: (unable to access|expected flush) ', ret, re.M):
             return print()
 
 
 def GetRepoList(user_name):
-    url = f'https://api.github.com/users/{user_name}/repos?sort=updated&direction=asc&per_page=100'
+    url = f'https://api.github.com/users/{user_name}/repos?sort=updated&direction=dsc&per_page=100'
     headers = {'Accept': 'application/vnd.github.v3+json'}
     request = urllib.request.Request(url, headers=headers)
     data = urllib.request.urlopen(request).read()
@@ -30,16 +30,13 @@ def GetRepoList(user_name):
 
 def GetRepo(user_name, repo_name):
     url = f'https://github.com/{user_name}/{repo_name}/'
-    if os.path.isdir(repo_name):
-        os.chdir(repo_name)
-        RetryPopen(f'git pull')
-        os.chdir('..')
+    if os.path.isdir(f'../{repo_name}'):
+        RetryPopen(f'git.exe pull', f'../{repo_name}')
     else:
-        RetryPopen(f'git clone {url}')
+        RetryPopen(f'git.exe clone {url}', '..')
 
 
 if __name__ == '__main__':
-    os.chdir('..')
     user_name = 'znsoooo'
     for repo_name in GetRepoList(user_name):
         print(f"Repo: '{repo_name}'")
